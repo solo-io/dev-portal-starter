9e4f4aafb2f2b2b7cf2170c099cec3259106e846
Nicholas Bucher <behappy54321@gmail.com>
2023-02-15 14:34:31 -0500

    updated rtk query example

--- projects/ui/src/redux/store.ts
+++ projects/ui/src/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { pokemonApi } from "./services/api";
-// ...
 
 export const store = configureStore({
   reducer: {
@@ -12,6 +12,10 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(pokemonApi.middleware),
 });
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
