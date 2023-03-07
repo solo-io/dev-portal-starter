bbc7dab73a53296aa42584151f4382fce78256f9
EW-Solo <ew-solo@EW-Solos-MacBook-Pro.local>
2023-03-07 15:33:53 -0500

    clickable list items

--- projects/ui/src/Components/ApisList/ApiSummaryListCard.tsx
+++ projects/ui/src/Components/ApisList/ApiSummaryListCard.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { API } from "../../Apis/api-types";
 import { Icon } from "../../Assets/Icons";
 
@@ -6,29 +7,31 @@ export function ApiSummaryListCard({ key, api }: { key?: string; api: API }) {
   // Render
   //
   return (
-    <div key={key} className="apiListCard">
-      <div className="content">
-        <div className="majorIconHolder">
-          <Icon.Bug />
+    <NavLink to={`/api-details/${api.apiId}`}>
+      <div key={key} className="apiListCard">
+        <div className="content">
+          <div className="majorIconHolder">
+            <Icon.Bug />
+          </div>
+          <div className="details">
+            <div>
+              <h4 className="title">{api.title}</h4>
+              <div className="description">{api.description}</div>
+            </div>
+          </div>
         </div>
-        <div className="details">
-          <div>
-            <h4 className="title">{api.title}</h4>
-            <div className="description">{api.description}</div>
+        <div className="footer">
+          <div className="metaInfo">
+            <Icon.SmallCodeGear />
+            <div className="typeTitle" aria-label="API Type">
+              OpenAPI
+            </div>
+          </div>
+          <div className="typeIcon">
+            <Icon.OpenApiIcon />
           </div>
         </div>
       </div>
-      <div className="footer">
-        <div className="metaInfo">
-          <Icon.SmallCodeGear />
-          <div className="typeTitle" aria-label="API Type">
-            OpenAPI
-          </div>
-        </div>
-        <div className="typeIcon">
-          <Icon.OpenApiIcon />
-        </div>
-      </div>
-    </div>
+    </NavLink>
   );
 }
