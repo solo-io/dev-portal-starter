d0053894240560f6fdca3335ce23acf7ddbbe1aa
Nicholas Bucher <behappy54321@gmail.com>
2023-04-19 16:11:49 -0400

    updated decorator

--- projects/ui/src/stories/decorators/decorators.tsx
+++ projects/ui/src/stories/decorators/decorators.tsx
@@ -2,7 +2,7 @@ import { Decorator } from "@storybook/react";
 
 export const appContentDecorator: Decorator = (Story) => {
   return (
-    <div className="AppContent" data-theme={"light"}>
+    <div className="AppContainer" data-theme={"light"}>
       <Story />
     </div>
   );
