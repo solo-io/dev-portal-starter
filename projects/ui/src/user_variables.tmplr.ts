9c8b4fe4f7e6660e4406e22b9a615aaee6ee1113
Nicholas Bucher <behappy54321@gmail.com>
2023-04-21 14:49:48 -0400

    cleanup

--- projects/ui/src/user_variables.tmplr.ts
+++ projects/ui/src/user_variables.tmplr.ts
@@ -10,11 +10,4 @@
 //
 // Project Settings
 //
-// export const projectName = "{{ tmplr.project_name }}";
 export const companyName = "{{ tmplr.company_name }}";
-
-//
-// Colors
-//
-// export const primaryColor = "{{ tmplr.primary_color }}";
-// export const secondaryColor = "{{ tmplr.secondary_color }}";
