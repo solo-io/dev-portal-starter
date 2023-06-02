d53a1cdb7ad8eaa25f473b33e1a9e85c286d4d7c
Nicholas Bucher <behappy54321@gmail.com>
2023-06-02 15:13:38 -0400

    Added swagger fix for apis with periods in their name.

--- projects/ui/src/Components/ApiDetails/SwaggerDisplay.tsx
+++ projects/ui/src/Components/ApiDetails/SwaggerDisplay.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SwaggerUIConstructor from "swagger-ui";
 import "swagger-ui/dist/swagger-ui.css";
 import { APISchema } from "../../Apis/api-types";
+
+const sanitize = (id: string) => id.replaceAll(".", "-");
 
 export function SwaggerDisplay({
   spec,
@@ -10,15 +12,27 @@ export function SwaggerDisplay({
   spec: APISchema | undefined;
   apiId: string;
 }) {
+  // The sanitized dom_id, where all periods are replaced with dashes. This fixes issues where Swagger tries
+  // doing a `querySelector` which fails, due to it treating the period as a class selector, and not part of the ID itself.
+  const [sanitizedDomId, setSanitizedDomId] = useState<string>(sanitize(apiId));
+  useEffect(() => {
+    const newSanitizedId = sanitize(apiId);
+    if (sanitizedDomId !== newSanitizedId) {
+      setSanitizedDomId(newSanitizedId);
+    }
+  }, [apiId, sanitizedDomId]);
+
   useEffect(() => {
     SwaggerUIConstructor({
       spec: spec,
-      dom_id: `#display-swagger-${apiId}`,
+      dom_id: `#display-swagger-${sanitizedDomId}`,
       withCredentials: true,
       deepLinking: true,
       syntaxHighlight: { activate: false },
     });
-  }, [apiId]);
+  }, [sanitizedDomId, spec]);
 
-  return <div aria-label="Schema Display" id={`display-swagger-${apiId}`} />;
+  return (
+    <div aria-label="Schema Display" id={`display-swagger-${sanitizedDomId}`} />
+  );
 }
