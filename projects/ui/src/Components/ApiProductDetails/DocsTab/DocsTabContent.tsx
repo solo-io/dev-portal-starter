db3e27a78f6a644656994a779cd55ce2da99a695
Nicholas Bucher <behappy54321@gmail.com>
2024-03-19 12:24:18 -0400

    Updated docs tab to use markdown
    Signed-off-by: Nicholas Bucher <behappy54321@gmail.com>

--- projects/ui/src/Components/ApiProductDetails/DocsTab/DocsTabContent.tsx
+++ projects/ui/src/Components/ApiProductDetails/DocsTab/DocsTabContent.tsx
@@ -1,11 +1,43 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { Box } from "@mantine/core";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import { ApiVersion } from "../../../Apis/api-types";
+import { CardStyles } from "../../../Styles/shared/Card.style";
+
+const MarkdownOuterContainer = styled.div(
+  ({ theme }) => css`
+    * {
+      margin: revert;
+      padding: revert;
+      font-family: revert;
+      font-weight: revert;
+    }
+    blockquote p {
+      color: ${theme.augustGrey};
+    }
+  `
+);
 
 const DocsTabContent = ({
   selectedApiVersion,
 }: {
   selectedApiVersion: ApiVersion;
 }) => {
-  return <div>DocsTabContent</div>;
+  return (
+    <Box pb={"60px"}>
+      <CardStyles.Card>
+        <Box p={"30px"}>
+          <MarkdownOuterContainer>
+            <Markdown remarkPlugins={[remarkGfm]}>
+              {selectedApiVersion.documentation}
+            </Markdown>
+          </MarkdownOuterContainer>
+        </Box>
+      </CardStyles.Card>
+    </Box>
+  );
 };
 
 export default DocsTabContent;
