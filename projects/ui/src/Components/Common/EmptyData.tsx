64ce0e26e9f130140381d3a317403b50f1c3bf03
Nicholas Bucher <behappy54321@gmail.com>
2024-04-09 14:05:55 -0400

    added rejected filter to admin subscriptions, and removed empty data animation
    Signed-off-by: Nicholas Bucher <behappy54321@gmail.com>

--- projects/ui/src/Components/Common/EmptyData.tsx
+++ projects/ui/src/Components/Common/EmptyData.tsx
@@ -1,125 +1,4 @@
-import { css } from "@emotion/react";
-import styled from "@emotion/styled";
-
-const LoadingContainer = styled.div(
-  ({ theme }) => css`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    padding-bottom: 30px;
-
-    // A pretty spinner while they wait...
-    // .inner and .outer define the two circling balls, and
-    //   their 'color's can be changed here.
-    .loadingSpinner,
-    .emptyCircle {
-      position: relative;
-
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 100px;
-      height: 100px;
-
-      .chasePlate {
-        position: absolute;
-        border-radius: 50%;
-        border-style: solid;
-        animation: animateCircles 3s linear infinite;
-
-        &.outer {
-          width: 100%;
-          height: 100%;
-          color: ${theme.primary};
-          border-color: currentColor transparent transparent currentColor;
-          border-width: 0.2em 0.2em 0em 0em;
-          --deg: -45deg;
-          animation-direction: normal;
-        }
-
-        &.inner {
-          width: 65%;
-          height: 65%;
-          color: ${theme.secondary};
-          border-color: currentColor currentColor transparent transparent;
-          border-width: 0.2em 0em 0em 0.2em;
-          --deg: -135deg;
-          animation-direction: reverse;
-        }
-
-        .circle {
-          position: absolute;
-          width: 50%;
-          height: 0.1em;
-          top: 50%;
-          left: 50%;
-          background-color: transparent;
-          transform: rotate(var(--deg));
-          transform-origin: left;
-        }
-
-        .circle::before {
-          position: absolute;
-          content: "";
-          /* width: 1em;
-          height: 1em;
-          top: -0.5em;
-          right: -0.5em; */
-          width: 12px;
-          height: 12px;
-          top: -6px;
-          right: -8px;
-          background-color: currentColor;
-          border-radius: 50%;
-        }
-      }
-    }
-    .emptyCircle {
-      .chasePlate {
-        animation: animateCircles 1.3s linear;
-
-        &.inner,
-        &.outer {
-          border-width: 0.2em;
-          border-color: currentColor;
-        }
-        &.inner {
-          --deg: -90deg;
-        }
-        &.outer {
-          --deg: -90deg;
-        }
-      }
-    }
-
-    .loadingMessage,
-    .emptyMainMessage,
-    .emptyDetailsMessage {
-      padding-top: 20px;
-      text-align: center;
-      font-size: 18px;
-      font-weight: 500;
-
-      color: ${theme.defaultColoredText};
-    }
-    .emptyDetailsMessage {
-      padding-top: 8px;
-      font-size: 16px;
-      line-height: 20px;
-      font-weight: 400;
-    }
-
-    //
-    // Animations
-    //
-    @keyframes animateCircles {
-      to {
-        transform: rotate(1turn);
-      }
-    }
-  `
-);
+import { Flex, Text } from "@mantine/core";
 
 type EmptyDataProps =
   | {
@@ -131,25 +10,19 @@ type EmptyDataProps =
     };
 export function EmptyData(props: EmptyDataProps) {
   return (
-    <LoadingContainer aria-hidden="true">
-      <div className="emptyCircle">
-        <div className="chasePlate outer">
-          <div className="circle"></div>
-        </div>
-        <div className="chasePlate inner">
-          <div className="circle"></div>
-        </div>
-      </div>
-      {"topicMessageOverride" in props ? (
-        <div className="emptyMainMessage">{props.topicMessageOverride}</div>
-      ) : (
-        <div className="emptyMainMessage">
-          No {props.topic} results were found
-        </div>
-      )}
+    <Flex justify={"center"}>
+      <Text color="gray.6" italic>
+        {"topicMessageOverride" in props ? (
+          <>{props.topicMessageOverride}</>
+        ) : (
+          <>No {props.topic} results were found</>
+        )}
+      </Text>
       {"message" in props && !!props.message && (
-        <div className="emptyDetailsMessage">{props.message}</div>
+        <Text color="gray.6" italic>
+          {props.message}
+        </Text>
       )}
-    </LoadingContainer>
+    </Flex>
   );
 }
