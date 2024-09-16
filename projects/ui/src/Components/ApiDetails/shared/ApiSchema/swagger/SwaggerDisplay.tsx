fc7478480dc1f2c91fe693f7a7c9f079983177d5
Nicholas Bucher <behappy54321@gmail.com>
2024-09-16 14:23:42 -0400

    notes
    Signed-off-by: Nicholas Bucher <behappy54321@gmail.com>

--- projects/ui/src/Components/ApiDetails/shared/ApiSchema/swagger/SwaggerDisplay.tsx
+++ projects/ui/src/Components/ApiDetails/shared/ApiSchema/swagger/SwaggerDisplay.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import SwaggerUIConstructor from "swagger-ui";
 import "swagger-ui/dist/swagger-ui.css";
 import { ApiVersionSchema } from "../../../../../Apis/api-types";
+import { AuthContext } from "../../../../../Context/AuthContext";
 import { swaggerConfigURL } from "../../../../../user_variables.tmplr";
 import { SwaggerDisplayContainer } from "./SwaggerDisplay.style";
 
@@ -14,6 +15,8 @@ export function SwaggerDisplay({
   apiVersionSpec: ApiVersionSchema | undefined;
   apiVersionId: string;
 }) {
+  const { tokensResponse } = useContext(AuthContext);
+
   // The sanitized dom_id, where all periods are replaced with dashes. This fixes issues where Swagger tries
   // doing a `querySelector` which fails, due to it treating the period as a class selector, and not part of the ID itself.
   const [sanitizedDomId, setSanitizedDomId] = useState<string>(
@@ -27,13 +30,30 @@ export function SwaggerDisplay({
   }, [apiVersionId, sanitizedDomId]);
 
   useEffect(() => {
-    SwaggerUIConstructor({
+    const swaggerInstance = SwaggerUIConstructor({
       spec: apiVersionSpec,
       dom_id: `#display-swagger-${sanitizedDomId}`,
       withCredentials: true,
       deepLinking: true,
       configUrl: swaggerConfigURL !== "" ? swaggerConfigURL : undefined,
     });
+    // swaggerInstance.preauthorizeApiKey(
+    //   "Authorization",
+    //   tokensResponse?.access_token ?? ""
+    // );
+    /** TODO
+     * The first arg here (authDefinitionKey), refers to the key (api_key)
+     * in the `apiVersionSpec.components.securitySchemes` object.
+     * This looks like it is user defined, but has a specific (apiKey) type
+     * in the object... so will need to look at this some more.
+     **/
+    if (!!tokensResponse?.access_token) {
+      // TODO: This might need to be a type of oauth2 flow, a basic auth flow, or something else.
+      swaggerInstance.preauthorizeApiKey(
+        "api_key",
+        tokensResponse.access_token
+      );
+    }
   }, [sanitizedDomId, apiVersionSpec]);
 
   return (
