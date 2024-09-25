140215eb259cef29c2abe812aed4ce02ffe80b48
Nicholas Bucher <behappy54321@gmail.com>
2024-09-25 16:49:27 -0400

    starting on metadata
    Signed-off-by: Nicholas Bucher <behappy54321@gmail.com>

--- projects/ui/src/Components/Apps/AppMetadata/AppMetadataDisplay.tsx
+++ projects/ui/src/Components/Apps/AppMetadata/AppMetadataDisplay.tsx
@@ -1,18 +1,39 @@
 import { Box, Text } from "@mantine/core";
 import { App } from "../../../Apis/api-types";
+import { useIsAdmin } from "../../../Context/AuthContext";
 import { colors } from "../../../Styles";
 import { borderRadiusConstants } from "../../../Styles/constants";
 
 const AppMetadataDisplay = ({ app }: { app: App }) => {
+  const isAdmin = useIsAdmin();
+
+  if (!app.metadata && !isAdmin) {
+    return null;
+  }
   return (
     <Box
       sx={{
-        border: "1px solid " + colors.splashBlue,
+        border: "1px solid " + colors.splashBlueDark10,
+        backgroundColor: colors.dropBlue,
         borderRadius: borderRadiusConstants.small,
       }}
-      p="20px"
+      p="10px 15px"
+      my="5px"
     >
-      <Text size="md">Metadata for {app.name}</Text>
+      {!app.metadata ? (
+        <>
+          <Text size="md" weight={500}>
+            No Metadata
+          </Text>
+        </>
+      ) : (
+        <>
+          <Text size="md" weight={500}>
+            Metadata
+          </Text>
+          <Text size="sm">{JSON.stringify(app.metadata)}</Text>
+        </>
+      )}
     </Box>
   );
 };
